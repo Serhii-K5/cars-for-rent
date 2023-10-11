6e2c8bcb8663782728a2d1041ba233f1844dcfa2
Serhii-K5 <116863810+Serhii-K5@users.noreply.github.com>
2023-10-11 09:27:43 +0000

    Deploying to gh-pages from @ Serhii-K5/cars-for-rent@e7a80e6063e376bc1a9a13afdba3b17f5af74b7b 🚀

--- src/redux/selectors.js
+++
@@ -1,7 +0,0 @@
-export const selectAdverts = (state) => state.adverts.items;
-
-export const selectFavorites = (state) => state.favorites.items;
-
-export const selectIsLoading = (state) => state.adverts.isLoading;
-
-export const selectError = (state) => state.adverts.error;
