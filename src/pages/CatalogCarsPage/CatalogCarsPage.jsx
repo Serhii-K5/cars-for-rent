ee4b5f973bc689cba1852a3241052cb3772f656b
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-11-13 16:54:55 +0200

    Add pagination

--- src/pages/CatalogCarsPage/CatalogCarsPage.jsx
+++ src/pages/CatalogCarsPage/CatalogCarsPage.jsx
@@ -7,47 +7,58 @@ import { selectAdverts } from "redux/selectors";
 
 import {
   Ul,
-  Button,
+  // Button,  // V1 - пагінація з додаванням елементів по натисканню кнопки
+  DivShift,   // V2 - пагінація з заміщенням елементів
+  DivPagination,  // V2 - пагінація з заміщенням елементів
+  DivPage,    // V2 - пагінація з заміщенням елементів
 } from './CatalogCarsPage.styled';
 
 import FilterPanel from 'components/FilterPanel/FilterPanel';
 
 
+// V1 - пагінація з додаванням елементів по натисканню кнопки
 // const CatalogCarsPage = () => {
 //   const dispatch = useDispatch();
 //   const adverts = useSelector(selectAdverts);
 //   const [filteredData, setFilteredData] = useState(adverts);
-//   // const [activePage, setActivePage] = useState(0);
+
+//   const [activePage, setActivePage] = useState(0);
+//   const [activeFilter, setActiveFilter] = useState(false);
   
 //   useEffect(() => {
 //     dispatch(fetchAdverts());
 //   }, [dispatch]);
 
 //   const handleFilter = filteredData => {
+//     setActiveFilter(true);
+//     setActivePage(0);
 //     setFilteredData(filteredData);
 //   };
+
+//   const onClickPagination = () => {
+//     // setPaginationArray(filteredData.slice(activePage, activePage + 8));
+//     setActivePage(activePage + 8);
+//   }
 
 //   return (
 //     <div style={{ padding: '50px' }}>
 //       <FilterPanel data={adverts} onFilter={handleFilter} />
+//       {filteredData.length === 0 && adverts.length > 0 && !activeFilter && setFilteredData(adverts)}
 //       {filteredData.length > 0 && (
 //         <Ul>
-//           {filteredData.map(item => (
-//             <li key={item.id}>
+//           {filteredData.map((item, index) => (            
+//             index < activePage + 8 && <li key={item.id}>
 //               <CarCard card={item} />
 //             </li>
-//           ))}
+//           ))};
 //         </Ul>
 //       )}
-//       {filteredData.length === 0 && adverts.length > 0 && (
-//         <Ul>
-//           {adverts.map(item => (
-//             <li key={item.id}>
-//               <CarCard card={item} />
-//             </li>
-//           ))} 
-//         </Ul>
-//       )}
+//       <Button
+//         onClick={onClickPagination}
+//         style={activePage + 8 > filteredData.length ? {display: "none"} : {display: "block"}}
+//       >
+//         Load more
+//       </Button>
 //     </div>
 //   );
 // };
@@ -55,14 +66,16 @@ import FilterPanel from 'components/FilterPanel/FilterPanel';
 // export default CatalogCarsPage;
 
 
+// V2 - пагінація з заміщенням елементів
 const CatalogCarsPage = () => {
   const dispatch = useDispatch();
   const adverts = useSelector(selectAdverts);
   const [filteredData, setFilteredData] = useState(adverts);
 
   const [activePage, setActivePage] = useState(0);
-  const [paginationArray, setPaginationArray] = useState([]);
   const [activeFilter, setActiveFilter] = useState(false);
+  // const [activeFilter, setActiveFilter] = useState(false);
+
   
   useEffect(() => {
     dispatch(fetchAdverts());
@@ -71,17 +84,34 @@ const CatalogCarsPage = () => {
   const handleFilter = filteredData => {
     setActiveFilter(true);
     setActivePage(0);
-    setFilteredData(filteredData);    
-    setPaginationArray(filteredData);
+    setFilteredData(filteredData);
   };
 
-  const onClickPagination = () => {
-    // setPaginationArray(filteredData.slice(activePage, activePage + 8));
-    setActivePage(activePage + 8);
-    // setPaginationArray(filteredData.slice(0, activePage + 8));
-    const page = activePage + 8;
-    setPaginationArray(filteredData.slice(0, page + 8));
-  }
+  const onClickIncrease  = () => {
+    activePage < filteredData.length / 8 && setActivePage(activePage + 1);
+  };
+  
+  const onClickDecrease  = () => {
+    activePage > 0 && setActivePage(activePage - 1);
+  };
+  
+  // const onClickButton1  = () => {
+  //   activePage > 0 && setActivePage(activePage - 1);
+  // };
+  
+  // const onClickButton2  = () => {
+  //   activePage > 0 && setActivePage(activePage - 1);
+  // };
+  
+  // const onClickButton3  = () => {
+  //   activePage > 0 && setActivePage(activePage - 1);
+  // };
+  
+  // const onClickButton4  = () => {
+  //   activePage > 0 && setActivePage(activePage - 1);
+  // };
+
+
 
   return (
     <div style={{ padding: '50px' }}>
@@ -89,25 +119,32 @@ const CatalogCarsPage = () => {
       {filteredData.length === 0 && adverts.length > 0 && !activeFilter && setFilteredData(adverts)}
       {filteredData.length > 0 && (
         <Ul>
-          {paginationArray.length === 0 && setPaginationArray(filteredData.slice(0, activePage + 8))}
-          {paginationArray.map(item => (
-            <li key={item.id}>
+          {filteredData.map((item, index) => (            
+            index < activePage * 8 && <li key={item.id}>
               <CarCard card={item} />
             </li>
-          ))}
+          ))};
         </Ul>
       )}
-      <Button
-        onClick={onClickPagination}
-        style={activePage + 8 > filteredData.length ? {display: "none"} : {display: "block"}}
-      >
-        Load more
-      </Button>
+      <DivPagination>
+        {activePage > 0 && <DivShift onClick={onClickDecrease}>{"<<"}</DivShift>}
+        {/* {activePage > 0 && <DivPage onClick={onClickDecrease} style={activePage > 0 && {backgroundColor: 'green'}}>{activePage}</DivPage>} */}
+        {activePage > 0 && <DivPage onClick={onClickDecrease}>{activePage}</DivPage>}
+        <DivPage>{activePage + 1}</DivPage>
+        {filteredData.length / 8 > activePage + 1 && <DivPage onClick={onClickIncrease}>{activePage + 2}</DivPage>}
+        {filteredData.length / 8 > activePage + 2 && <DivPage>{"..."}</DivPage>}     
+        <DivPage onClick={onClickIncrease}>{filteredData.length / 8}</DivPage>     
+        {filteredData.length / 8 > activePage + 2 && <DivShift onClick={onClickIncrease} >{">>"}</DivShift>}
+      </DivPagination>
     </div>
   );
 };
 
 export default CatalogCarsPage;
+
+
+
+
 
 // import {
 //   fetchDataWithPagination,
--- src/pages/CatalogCarsPage/CatalogCarsPage.styled.jsx
+++ src/pages/CatalogCarsPage/CatalogCarsPage.styled.jsx
@@ -13,6 +13,7 @@ export const Ul = styled.ul`
 `;
 
 export const Button = styled.button`
+  // display: none;
   margin: 0 auto;
   width: 270px;
   height: 50px;
@@ -27,4 +28,40 @@ export const Button = styled.button`
   border: none;
   border-radius: 12px;
   transition: background-color 250ms linear;
-`
+`;
+
+export const DivPagination = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DivShift = styled.div`
+  display: flex;
+  width: 50px;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const DivPage = styled.div`
+  display: flex;
+  width: 50px;
+  height: 40px;
+  border: 2px solid #000;
+  align-items: center;
+  justify-content: center;
+
+  &:active {
+    background-color: var(--bg-active-button-color);
+  }
+
+  &:focus {
+    background-color: var(--bg-active-button-color);
+  }
+
+  &:hover {
+    background-color: var(--bg-active-button-color);
+  }
+`;
+
